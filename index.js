e0f4594802fd955ca684b9c70ef3b84d695e8d02
blue butter <damiongallarza@gmail.com>
2023-06-20 17:25:08 -0700

    added questions to index.js

---
+++ index.js
@@ -0,0 +1,68 @@
+// TODO: Include packages needed for this application
+const inquirer = require("inquirer")
+const fs = require("fs")
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type:"input",
+        message:"Enter Project Title",
+        name:"title"
+    },
+    {
+        type:"input",
+        message:"Enter Project Description",
+        name:"description"
+    },
+    {
+        type:"input",
+        message:"Enter Project installation instructions",
+        name:"installationInstructions"
+    },
+    {
+        type:"input",
+        message:"Enter Project usage information",
+        name:"usageInformation"
+    },
+    {
+        type:"input",
+        message:"Enter Project contribution guidelines",
+        name:"contributionGuidelines"
+    },
+    {
+        type:"input",
+        message:"Enter Project test instructions",
+        name:"testInstructions"
+    },
+    {
+        type:"input",
+        message:"Enter your email address",
+        name:"emailAddress"
+    },
+    {
+        type:"list",
+        message:"Enter Project license",
+        name:"license",
+        choices:["MIT","ISC","APACHE 2.0","GPL2.0","None"]
+    },
+    {
+        type:"input",
+        message:"Enter GitHub username",
+        name:"GitHubUsername"
+    },
+    
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(questions)
+    .then(response => {
+        console.log(response)
+    })
+}
+
+// Function call to initialize app
+init();
